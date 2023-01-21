191d18b224944bced368cfa8aa1d5f1876d7ecab
Henry-Azer <henryazer@outlook.com>
2023-01-21 23:23:37 +0200

    implement jsis constants

---
+++ src/main/java/org/henry/jsis/constants/JSISConstants.java
@@ -0,0 +1,40 @@
+package org.henry.jsis.constants;
+
+/**
+ * @author Henry Azer
+ * @since 21/01/2023
+ **/
+public interface JSISConstants {
+    String JAVA_PACKAGE_PATH = "\\src\\main\\java\\";
+    String JAVA_FILE_EXTENSION = ".java";
+    String ONLY_CHARS_REGEX = "[a-zA-Z]+";
+
+    String INPUT_SERVICE_PATH = "Service Path: ";
+    String INPUT_SERVICE_NAME = "Service Name: ";
+    String INPUT_SERVICE_PACKAGE = "Service Package: ";
+    String INPUT_ANOTHER_SERVICE = "Another Service? (yes, no): ";
+
+    String INVALID_SERVICE_PACKAGE = "Invalid service package";
+    String INVALID_SERVICE_NAME = "Invalid service name";
+    String INVALID_SERVICE_PATH = "Invalid service path";
+
+    String DIRECTORY_CREATED = " directory created successfully";
+    String FILE_ALREADY_EXISTS = " file already exists";
+    String FILE = " file";
+
+    String BREAK_LINE = "-----------------------------------------";
+    String INIT_SERVICES_STARTED = "*** Initializing services -- started";
+    String INIT_SERVICES_ENDED = "*** Initializing services -- ended";
+    String INITIALIZE = "** Initialize ";
+    String STARTED = " -- started";
+    String ENDED = " -- ended";
+    String ERROR = "ERROR! - ";
+    String WARN = "WARN! - ";
+    String YES = "yes";
+    String SPACE = " ";
+    String HYPHEN_SPACE = "- ";
+
+    String CREATOR = "-------> Creator : Henry Azer XD <-------";
+    String JSIS = "---> Java Service Initializer Script <---";
+    String DATE = "----------> Date : 20/01/2023 <----------";
+}
